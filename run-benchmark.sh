e46eb230daea35a4815f6a4385df3f942d424f74
Marjan Povolni <marian.povolny@gmail.com>
2013-01-31 10:28:42 +0100

    Finished python example setup in benchmark script

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -8,9 +8,12 @@
 #       filter the results.
 
 
+echo Preparing test...
+
 # Create folder for all test executables
-echo Creating executables...
 mkdir -p build
+
+
 
 # Java test setup
 
@@ -22,7 +25,6 @@ if [ $? -ne 0 ]; then
   echo !!! Java has to be installed to successfully run this benchmark
   exit 1
 fi
-
 
 export BIOJAVA_VERSION=3.0.5
 export JAVA_EXEC_DIR=build/java
@@ -47,19 +49,67 @@ if [ ! -f "$JAVA_EXEC_DIR/biojava3-genome-$BIOJAVA_VERSION.jar" ]; then
 fi
 
 export CLASSPATH=$JAVA_EXEC_DIR:$JAVA_EXEC_DIR/biojava3-core-$BIOJAVA_VERSION.jar:$JAVA_EXEC_DIR/biojava3-genome-$BIOJAVA_VERSION.jar
-echo "    Building FatureCounter.java..."
+echo "    Building FeatureCounter.java..."
 javac -d $JAVA_EXEC_DIR java/FeatureCounter.java
 if [ ! -f $JAVA_EXEC_DIR/FeatureCounter.class ]; then
   echo !!! Failed to build Java benchmark
   exit 1
 fi
 
-# Python test setup
 
+
+# Python test setup
+export PYTHON_COMMAND=python
+export BIOPYTHON_VERSION=1.60
+export PYTHON_EXEC_DIR=build/python
+export PYTHONPATH=build/python/bcbb/gff:build/python/biopython-1.60
+
+echo "  Setting up Biopython example..."
+
+mkdir -p $PYTHON_EXEC_DIR
+
+echo "    Downloading Biopython $BIOPYTHON_VERSION..."
+if [ ! -f "$PYTHON_EXEC_DIR/biopython-$BIOPYTHON_VERSION.tar.gz" ]; then
+  wget --directory-prefix=$PYTHON_EXEC_DIR http://biopython.org/DIST/biopython-$BIOPYTHON_VERSION.tar.gz
+  if [ $? -ne 0 ]; then
+    echo !!! Failed to download Biopython $BIOPYTHON_VERSION! Maybe a network issue?
+    exit 1
+  fi
+fi
+
+echo "    Unpacking Biopython..."
+if [ -d $PYTHON_EXEC_DIR/biopython-$BIOPYTHON_VERSION ]; then
+  rm -Rf $PYTHON_EXEC_DIR/biopython-$BIOPYTHON_VERSION
+fi
+tar -zxf $PYTHON_EXEC_DIR/biopython-$BIOPYTHON_VERSION.tar.gz -C $PYTHON_EXEC_DIR
+if [ $? -ne 0 ]; then
+  echo !!! Failed to unpack Biopython $BIOPYTHON_VERSION
+  exit 1
+fi
+
+echo "    Downloading GFF parser..."
+if [ ! -d "$PYTHON_EXEC_DIR/bcbb" ]; then
+  git clone https://github.com/chapmanb/bcbb.git $PYTHON_EXEC_DIR/bcbb
+  if [ $? -ne 0 ]; then
+    echo !!! Failed to download GFF parser for Python from GitHub
+    exit 1
+  fi
+fi
+git --work-tree=$PYTHON_EXEC_DIR/bcbb --git-dir=$PYTHON_EXEC_DIR/bcbb/.git checkout 976627ee26ecb4d3f7a4100ce4745f3e968232ad
+if [ $? -ne 0 ]; then
+  echo !!! Failed to checkout the correct revision of bcbb
+  exit 1
+fi
+
+
+
+# Ruby setup
 
 
 
 
 
 # Running the benchmark
+
 # java FeatureCounter sample.gff
+# python python/bcbio-count-records.py ../../test_data/flybase/FB2011_07/dana-all-r1.3.gff
